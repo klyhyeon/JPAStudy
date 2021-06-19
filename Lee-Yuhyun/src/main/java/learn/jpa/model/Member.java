2f22d7c0a103a7758beefdbb3f3306c6f7ed9c95
yuhyeon <klyhyeon@gmail.com>
2021-06-19 21:53:39 +0900

    docs : add comment to legacy code
    - about Serializable

--- Lee-Yuhyun/src/main/java/learn/jpa/model/Member.java
+++ Lee-Yuhyun/src/main/java/learn/jpa/model/Member.java
@@ -1,9 +1,6 @@
 package learn.jpa.model;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,9 +8,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
-@Entity @Getter
+@Entity
+@Setter @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member implements Serializable {
+    //receiver가 deserialize할 때 sender가 보낸 클래스와 같은지 검증하기 위해 UID를 사용
     private static final long serialVersionUID = 3990803224604257521L;
     
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
