17de4862c8f0fa67fafe022b1afc11d2863984bb
siro <71188307+shirohoo@users.noreply.github.com>
2021-06-19 15:10:14 +0900

    fix: Chapter02 - JPA getting started (#5)
    * fix: Chapter02 - JPA getting started
    
    * Rename 2장_JPA시작.md to 2장_JPA getting started.md

--- Han-Changhun/src/test/java/learn/jpa/model/MemberTest.java
+++ Han-Changhun/src/test/java/learn/jpa/model/MemberTest.java
@@ -1,19 +1,40 @@
 package learn.jpa.model;
 
-import learn.jpa.repository.MemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 class MemberTest {
-    @Autowired
-    MemberRepository repository;
+    @PersistenceUnit
+    EntityManagerFactory emf;
+    
+    EntityManager em;
+    EntityTransaction tx;
+    
+    @BeforeEach
+    void setUp() {
+        em = emf.createEntityManager();
+        tx = em.getTransaction();
+    }
+    
+    @AfterEach
+    void tearDown() {
+        em.close();
+        emf.close();
+    }
     
     @Test
     void memberTest() throws Exception {
+        tx.begin();
         // given
         Member member = Member.builder()
                               .name("홍길동")
@@ -21,9 +42,14 @@ class MemberTest {
                               .build();
         
         // when
-        Member save = repository.save(member);
+        em.persist(member);
+        em.flush();
+        
+        Member findMember = em.find(Member.class, member.getId());
+        
         
         // then
-        assertThat(save).isSameAs(member);
+        assertThat(findMember).isSameAs(member);
+        tx.commit();
     }
 }
