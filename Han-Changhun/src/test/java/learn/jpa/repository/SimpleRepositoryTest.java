7f939a5de2f16279ac60c01febea1116aff44958
siro <71188307+shirohoo@users.noreply.github.com>
2021-06-20 23:23:36 +0900

    feat: Practiced query methods of JpaRepository 3 (#10)
    * feat: Data initialize and Data-JPA learning test
    
    * feat: Data initialize and Data-JPA learning test 2
    
    * feat: Practiced query methods of JpaRepository
    
    * feat: Practiced query methods of JpaRepository 2
    
    * feat: Practiced query methods of JpaRepository 3

--- Han-Changhun/src/test/java/learn/jpa/repository/SimpleRepositoryTest.java
+++ Han-Changhun/src/test/java/learn/jpa/repository/SimpleRepositoryTest.java
@@ -22,7 +22,7 @@ class SimpleRepositoryTest {
     MemberRepository memberRepository;
 
     @Test
-    @DisplayName("Member_1번을_조회한다")
+    @DisplayName("Member_1번을_조회")
     void findById() {
         Member member = memberRepository.findById(1L)
                                         .orElseThrow(NoSuchElementException::new);
@@ -32,7 +32,7 @@ class SimpleRepositoryTest {
     }
 
     @Test
-    @DisplayName("Member_1번_3번을_조회한다")
+    @DisplayName("Member_1번_3번을_조회")
     void findAllById() {
         List<Member> members = memberRepository.findAllById(Lists.newArrayList(1L, 3L));
 
@@ -43,7 +43,7 @@ class SimpleRepositoryTest {
     }
 
     @Test
-    @DisplayName("Member_초기_데이터는_5명이다")
+    @DisplayName("Member_초기_데이터는_5명")
     void findAll() {
         List<Member> members = memberRepository.findAll();
 
@@ -57,7 +57,7 @@ class SimpleRepositoryTest {
     }
 
     @Test
-    @DisplayName("Member_1번을_제거한다")
+    @DisplayName("Member_1번을_제거")
     void deleteById() {
         memberRepository.deleteById(1L);
 
@@ -72,7 +72,7 @@ class SimpleRepositoryTest {
     }
 
     @Test
-    @DisplayName("Member_1번_3번을_제거한다")
+    @DisplayName("Member_1번_3번을_제거")
     void deleteAllById() {
         memberRepository.deleteAllById(Lists.newArrayList(1L, 3L));
 
@@ -86,7 +86,7 @@ class SimpleRepositoryTest {
     }
 
     @Test
-    @DisplayName("Member_전체제거한다")
+    @DisplayName("Member_전체제거")
     void deleteAll() {
         memberRepository.deleteAll();
 
@@ -96,7 +96,7 @@ class SimpleRepositoryTest {
     }
 
     @Test
-    @DisplayName("Member_배치_1번_3번을_제거한다")
+    @DisplayName("Member_Batch_1번_3번을_제거")
     void deleteAllByIdInBatch() {
         memberRepository.deleteAllByIdInBatch(Lists.newArrayList(1L, 3L));
 
@@ -110,7 +110,7 @@ class SimpleRepositoryTest {
     }
 
     @Test
-    @DisplayName("Member_배치_전체제거한다")
+    @DisplayName("Member_Batch_전체제거")
     void deleteAllInBatch() {
         memberRepository.deleteAllInBatch();
 
@@ -120,14 +120,14 @@ class SimpleRepositoryTest {
     }
 
     @Test
-    @DisplayName("Member_1번이_존재하는지_확인한다")
+    @DisplayName("Member_1번이_존재하는지_확인")
     void existsById() {
         boolean exists = memberRepository.existsById(1L);
         assertThat(exists).isTrue();
     }
 
     @Test
-    @DisplayName("Member_전체수를_조회한다")
+    @DisplayName("Member_전체수를_조회")
     void count() {
         long count = memberRepository.count();
         assertThat(count).isEqualTo(5);
@@ -141,7 +141,7 @@ class SimpleRepositoryTest {
      * the size of the page to be returned, must be greater than 0. <br/>
      */
     @Test
-    @DisplayName("Page_API_학습테스트한다")
+    @DisplayName("Page_API")
     void page() {
         Page<Member> members = memberRepository.findAll(PageRequest.of(1, 3));
         Pageable pageable = members.getPageable();
@@ -166,7 +166,7 @@ class SimpleRepositoryTest {
     }
 
     @Test
-    @DisplayName("Example_API_사용하여_조건검색을_학습테스트한다")
+    @DisplayName("Example_API_조건검색")
     void exampleFindAll() {
         ExampleMatcher matcher = matching()
                 .withIgnorePaths("age") // age 는 무시하고 검색한다
@@ -193,7 +193,7 @@ class SimpleRepositoryTest {
      * @see "Han-Changhun/src/test/resources/query-method-1.png"
      */
     @Test
-    @DisplayName("Query_Methods_실습_읽기_접두사")
+    @DisplayName("Query_Methods_조회_접두사")
     void queryMethodsV1() {
         Member member = Member.createMember("tester", 77);
         Member tester = memberRepository.saveAndFlush(member);
@@ -212,7 +212,7 @@ class SimpleRepositoryTest {
      * @see "Han-Changhun/src/test/resources/query-method-1.png"
      */
     @Test
-    @DisplayName("Query_Methods_TOP_조회")
+    @DisplayName("Query_Methods_Top_조회")
     void queryMethodsV2() {
         // id=1 siro 와 id=6 siro 가 존재하는 상황에서
         // limit query 를 사용하여 id 우선순위가 더 높은 데이터를 조회한다
@@ -235,10 +235,10 @@ class SimpleRepositoryTest {
     /**
      * 참고자료 경로
      *
-     * @see "Han-Changhun/src/test/resources/query-method-1.png"
+     * @see "Han-Changhun/src/test/resources/query-method-2.png"
      */
     @Test
-    @DisplayName("Query_Methods_AND_조회")
+    @DisplayName("Query_Methods_And_조회")
     void queryMethodsV3() {
         Member member = Member.createMember("siro", 77);
         memberRepository.saveAndFlush(member); // id=6 siro save
@@ -252,10 +252,10 @@ class SimpleRepositoryTest {
     /**
      * 참고자료 경로
      *
-     * @see "Han-Changhun/src/test/resources/query-method-1.png"
+     * @see "Han-Changhun/src/test/resources/query-method-2.png"
      */
     @Test
-    @DisplayName("Query_Methods_OR_조회")
+    @DisplayName("Query_Methods_Or_조회")
     void queryMethodsV4() {
         Member member = Member.createMember("siro", 25);
         memberRepository.saveAndFlush(member); // id=6 siro save
@@ -267,5 +267,173 @@ class SimpleRepositoryTest {
                                      tuple("dennis", 25),
                                      tuple("siro", 25))
                            .size().isEqualTo(3);
+    }
+
+    /**
+     * 참고자료 경로
+     *
+     * @see "Han-Changhun/src/test/resources/query-method-2.png"
+     */
+    @Test
+    @DisplayName("Query_Methods_After_조회(초과)")
+    void queryMethodsV5() {
+        List<Member> members = memberRepository.findByIdAfter(1L);
+
+        assertThat(members).extracting("name", "age")
+                           .contains(tuple("sophia", 32),
+                                     tuple("dennis", 25),
+                                     tuple("james", 41),
+                                     tuple("michael", 33))
+                           .size().isEqualTo(4);
+    }
+
+    /**
+     * 참고자료 경로
+     *
+     * @see "Han-Changhun/src/test/resources/query-method-2.png"
+     */
+    @Test
+    @DisplayName("Query_Methods_After_조회(이상)")
+    void queryMethodsV6() {
+        List<Member> members = memberRepository.findByIdGreaterThanEqual(1L);
+
+        assertThat(members).extracting("name", "age")
+                           .contains(tuple("siro", 29),
+                                     tuple("sophia", 32),
+                                     tuple("dennis", 25),
+                                     tuple("james", 41),
+                                     tuple("michael", 33))
+                           .size().isEqualTo(5);
+    }
+
+    /**
+     * 참고자료 경로
+     *
+     * @see "Han-Changhun/src/test/resources/query-method-2.png"
+     */
+    @Test
+    @DisplayName("Query_Methods_Before_조회(미만)")
+    void queryMethodsV7() {
+        List<Member> members = memberRepository.findByIdBefore(5L);
+
+        assertThat(members).extracting("name", "age")
+                           .contains(tuple("siro", 29),
+                                     tuple("sophia", 32),
+                                     tuple("dennis", 25),
+                                     tuple("james", 41))
+                           .size().isEqualTo(4);
+    }
+
+    /**
+     * 참고자료 경로
+     *
+     * @see "Han-Changhun/src/test/resources/query-method-2.png"
+     */
+    @Test
+    @DisplayName("Query_Methods_Before_조회(이하)")
+    void queryMethodsV8() {
+        List<Member> members = memberRepository.findByIdIsLessThanEqual(5L);
+
+        assertThat(members).extracting("name", "age")
+                           .contains(tuple("siro", 29),
+                                     tuple("sophia", 32),
+                                     tuple("dennis", 25),
+                                     tuple("james", 41),
+                                     tuple("michael", 33))
+                           .size().isEqualTo(5);
+    }
+
+    /**
+     * 참고자료 경로
+     *
+     * @see "Han-Changhun/src/test/resources/query-method-2.png"
+     */
+    @Test
+    @DisplayName("Query_Methods_Between_조회")
+    void queryMethodsV9() {
+        List<Member> members = memberRepository.findByAgeBetween(20, 30);
+
+        assertThat(members).extracting("name", "age")
+                           .contains(tuple("siro", 29),
+                                     tuple("dennis", 25))
+                           .size().isEqualTo(2);
+    }
+
+    /**
+     * 참고자료 경로
+     *
+     * @see "Han-Changhun/src/test/resources/query-method-2.png"
+     */
+    @Test
+    @DisplayName("Query_Methods_NotNull_조회")
+    void queryMethodsV10() {
+        List<Member> members = memberRepository.findByIdIsNotNull();
+
+        assertThat(members).extracting("name", "age")
+                           .contains(tuple("siro", 29),
+                                     tuple("sophia", 32),
+                                     tuple("dennis", 25),
+                                     tuple("james", 41),
+                                     tuple("michael", 33))
+                           .size().isEqualTo(5);
+    }
+
+    /**
+     * 참고자료 경로
+     *
+     * @see "Han-Changhun/src/test/resources/query-method-2.png"
+     */
+    @Test
+    @DisplayName("Query_Methods_In_조회(Batch)")
+    void queryMethodsV11() {
+        List<Member> members = memberRepository.findByAgeIn(Lists.newArrayList(29, 32, 25));
+
+        assertThat(members).extracting("name", "age")
+                           .contains(tuple("siro", 29),
+                                     tuple("sophia", 32),
+                                     tuple("dennis", 25))
+                           .size().isEqualTo(3);
+    }
+
+    /**
+     * 참고자료 경로
+     *
+     * @see "Han-Changhun/src/test/resources/query-method-2.png"
+     */
+    @Test
+    @DisplayName("Query_Methods_Starting_조회")
+    void queryMethodsV12() {
+        Member siro = memberRepository.findByNameStartingWith("si").get(0);
+
+        assertThat(siro).extracting("name", "age")
+                        .containsExactly("siro", 29);
+    }
+
+    /**
+     * 참고자료 경로
+     *
+     * @see "Han-Changhun/src/test/resources/query-method-2.png"
+     */
+    @Test
+    @DisplayName("Query_Methods_Ending_조회")
+    void queryMethodsV13() {
+        Member siro = memberRepository.findByNameEndingWith("ro").get(0);
+
+        assertThat(siro).extracting("name", "age")
+                        .containsExactly("siro", 29);
+    }
+
+    /**
+     * 참고자료 경로
+     *
+     * @see "Han-Changhun/src/test/resources/query-method-2.png"
+     */
+    @Test
+    @DisplayName("Query_Methods_Containing_조회")
+    void queryMethodsV14() {
+        Member siro = memberRepository.findByNameContaining("ir").get(0);
+
+        assertThat(siro).extracting("name", "age")
+                        .containsExactly("siro", 29);
     }
 }
