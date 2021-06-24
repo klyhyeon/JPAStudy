874bbda966d6aa67e94396e75de7334349708253
rkdals213 <rkdals213@naver.com>
2021-06-24 19:07:28 +0900

    여러 테스트 수행

--- Kang-Minhyung/src/main/java/learn/jpa/model/Member.java
+++ Kang-Minhyung/src/main/java/learn/jpa/model/Member.java
@@ -5,26 +5,39 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
-@Entity @Getter
+@Entity
+@Table
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MEMBER_SEQ_TABLE",
+        pkColumnValue = "MEMBER_SEQ", allocationSize = 3
+)
 public class Member implements Serializable {
     private static final long serialVersionUID = 3990803224604257521L;
-    
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
     private String name;
     private int age;
-    
+    private String address;
+
     @Builder
-    public Member(Long id, String name, int age) {
+    public Member(Long id, String name, int age, String address) {
         this.id = id;
         this.name = name;
         this.age = age;
+        this.address = address;
+    }
+
+    public Member(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
     }
 }
--- Kang-Minhyung/src/test/java/learn/jpa/model/MemberTest.java
+++ Kang-Minhyung/src/test/java/learn/jpa/model/MemberTest.java
@@ -11,19 +11,86 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MemberTest {
     @Autowired
     MemberRepository repository;
-    
+
     @Test
     void memberTest() throws Exception {
         // given
         Member member = Member.builder()
-                              .name("홍길동")
-                              .age(30)
-                              .build();
-        
+                .name("홍길동")
+                .age(30)
+                .build();
+
         // when
         Member save = repository.save(member);
-        
+
         // then
         assertThat(save).isSameAs(member);
+    }
+
+    @Test
+    void generateIdTest1() {
+        Member member = Member.builder()
+                .id(10L)
+                .name("홍길동")
+                .age(30)
+                .address("서울시")
+                .build();
+
+        Member save = repository.save(member);
+
+        assertThat(save.getId()).isEqualTo(member.getId());
+    }
+
+    @Test
+    void generateIdTest2() {
+        Member member = Member.builder()
+                .id(10L)
+                .name("홍길동")
+                .age(30)
+                .address("서울시")
+                .build();
+
+        Member save = repository.save(member);
+
+        assertThat(save.getId()).isNotEqualTo(member.getId());
+    }
+
+    @Test
+    void generateIdTest3() {
+        Member member = Member.builder()
+                .id(10L)
+                .name("홍길동")
+                .age(30)
+                .address("서울시")
+                .build();
+
+        Member save = repository.save(member);
+
+        assertThat(save.getId()).isEqualTo(17);
+    }
+
+    @Test
+    void generateIdTest4() {
+        Member member = Member.builder()
+                .id(10L)
+                .name("홍길동")
+                .age(30)
+                .address("서울시")
+                .build();
+
+        Member save = repository.save(member);
+
+        Member member2 = Member.builder()
+                .id(10L)
+                .name("홍길동")
+                .age(30)
+                .address("서울시")
+                .build();
+
+        Member save2 = repository.save(member2);
+        System.out.println(save.getId());
+        System.out.println(save2.getId());
+
+        assertThat(save.getId()).isEqualTo(3);
     }
 }
