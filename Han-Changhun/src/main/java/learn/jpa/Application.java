36dcdfeb2b77b0b0263e1c3be3fee4a83fccf9e4
siro <71188307+shirohoo@users.noreply.github.com>
2021-06-24 21:01:31 +0900

    feat(Han): @Embeddable test (#17)

--- Han-Changhun/src/main/java/learn/jpa/Application.java
+++ Han-Changhun/src/main/java/learn/jpa/Application.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- Na-Taegeun/src/main/java/learn/jpa/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package learn.jpa.repository;
-
-import learn.jpa.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
