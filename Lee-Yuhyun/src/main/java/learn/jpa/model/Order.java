9560b3592dabc025cf4204dff5c49016c91b9fe3
yuhyeon <klyhyeon@gmail.com>
2021-06-28 09:49:50 +0900

    feat : 4장_실전예제

---
+++ Lee-Yuhyun/src/main/java/learn/jpa/model/Order.java
@@ -0,0 +1,29 @@
+package learn.jpa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private Orderstatus status;
+}
