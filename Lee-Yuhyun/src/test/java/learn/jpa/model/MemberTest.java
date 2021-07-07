a3d3cb2a22249f134dcd35ff5b209c50fdc17eeb
yuhyeon <klyhyeon@gmail.com>
2021-07-07 09:48:12 +0900

    feat : (working) 7장_고급매핑 - 조인 테이블부터 진행필요

--- Lee-Yuhyun/src/test/java/learn/jpa/model/MemberTest.java
+++ Lee-Yuhyun/src/test/java/learn/jpa/model/MemberTest.java
@@ -1,41 +1,41 @@
-//package learn.jpa.model;
-//
-//import learn.jpa.repository.MemberRepository;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-//
-//@DataJpaTest
-//class MemberTest {
-//    @Autowired
-//    MemberRepository memberRepository;
-//
-//    @Test
-//    void memberTest() throws Exception {
-//
+package learn.jpa.model;
+
+import learn.jpa.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@DataJpaTest
+class MemberTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void memberTest() throws Exception {
+
+        Member member = Member.builder()
+//                                .username("유현")
+//                                .age(30)
+                                .build();
+
+        Member newMember = memberRepository.save(member);
+
+//        assertThat(newMember.getAge()).isEqualTo(39);
+
+
+//        // given
 //        Member member = Member.builder()
-////                                .username("유현")
-////                                .age(30)
-//                                .build();
+//                              .username("홍길동")
+//                              .age(30)
+//                              .build();
 //
-//        Member newMember = memberRepository.save(member);
+//        // when
+//        Member save = repository.save(member);
 //
-////        assertThat(newMember.getAge()).isEqualTo(39);
-//
-//
-////        // given
-////        Member member = Member.builder()
-////                              .username("홍길동")
-////                              .age(30)
-////                              .build();
-////
-////        // when
-////        Member save = repository.save(member);
-////
-////        // then
-////        assertThat(save).isSameAs(member);
-//    }
-//}
+//        // then
+//        assertThat(save).isSameAs(member);
+    }
+}
