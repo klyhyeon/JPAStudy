a28dc10f4f2d9aa46829a39b06ac3986981e30ee
YuHyeon <klyhyeon@gmail.com>
2021-07-13 21:25:53 +0900

    chore : Lombok Refactoring
    - Remove @Data(!JPA), etc not used annotation

--- Lee-Yuhyun/src/main/java/exercise/jpa/Item.java
+++ Lee-Yuhyun/src/main/java/exercise/jpa/Item.java
@@ -1,22 +1,12 @@
 package exercise.jpa;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-
-@Data
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "DTYPE")
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public abstract class Item {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
