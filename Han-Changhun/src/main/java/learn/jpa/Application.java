987bdb2c3faebeb1152f984158e39b26be3723fe
shirohoo <71188307+shirohoo@users.noreply.github.com>
2021-07-24 17:08:12 +0900

    feat: Querydsl 셋업 및 JPQL 테스트코드를 작성 (#36)
    * feat: Querydsl을 셋업
    
    * feat: JPQL 테스트 코드를 작성
    
    * docs: Update PULL_REQUEST_TEMPLATE.md

--- Han-Changhun/src/main/java/learn/jpa/Application.java
+++ Han-Changhun/src/main/java/learn/jpa/Application.java
@@ -6,15 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-        return new PersistenceExceptionTranslationPostProcessor();
     }
 }
---
+++ chapter/10/10장_객체지향_쿼리언어_hch.md
@@ -0,0 +1,1 @@
+[JPQL](https://shirohoo.github.io/spring/spring-data-jpa/2021-07-22-jpa-8/)
