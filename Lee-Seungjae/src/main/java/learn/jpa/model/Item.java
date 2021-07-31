08304486bef6bc4980c89ef251d6e3c6e28c8dd5
SeungJae Lee <lsj8367@naver.com>
2021-07-31 12:25:51 +0900

    feat: 10.4 테스트용 엔티티 작성

---
+++ Lee-Seungjae/src/main/java/learn/jpa/model/Item.java
@@ -0,0 +1,29 @@
+package learn.jpa.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@ToString
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private int price;
+
+    @Builder
+    public Item(Long id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
