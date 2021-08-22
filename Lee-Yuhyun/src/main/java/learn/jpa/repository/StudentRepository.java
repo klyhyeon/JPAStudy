6ca2e717263760bc5254d5155033cb518785a514
YuHyeon <klyhyeon@gmail.com>
2021-08-22 17:29:15 +0900

    feat: 12장 스프링 데이터 JPA 예제코드

---
+++ Lee-Yuhyun/src/main/java/learn/jpa/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package learn.jpa.repository;
+
+import learn.jpa.model.ch12.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Page<Student> findByNameStartingWith(String name, Pageable pageable);
+}
---
+++ Lee-Yuhyun/src/test/java/learn/jpa/repository/StudentRepositoryTest.java
@@ -0,0 +1,33 @@
+package learn.jpa.repository;
+
+import learn.jpa.model.ch12.Student;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+@DataJpaTest
+class StudentRepositoryTest {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    @DisplayName("Pageable 테스트")
+    void pageableTest() {
+        //페이징 조건과 정렬 조건 설정
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name").ascending());
+        //given
+        String name = "yuhyeon";
+        Page<Student> result = studentRepository.findByNameStartingWith(name, pageRequest);
+        List<Student> students = result.getContent();
+        int totalPages = result.getTotalPages();
+        boolean hasNextPage = result.hasNext();
+    }
+
+}
